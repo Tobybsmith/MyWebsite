6734dbc82bb84f2ffa58701d745c0ce5f61190a4
Tobybsmith <tobybsmith121@gmail.com>
2021-12-12 21:42:30 -0500

    Added the html pages for cheese

---
+++ cheese/scripts/landing.js
@@ -0,0 +1,9 @@
+const signin = document.getElementById("login")
+const signup = document.getElementById("signup")
+
+signin.addEventListener('click', () => {
+    window.location.href = "./signin.html"
+})
+signup.addEventListener('click', () => {
+    window.location.href = "./signup.html"
+})

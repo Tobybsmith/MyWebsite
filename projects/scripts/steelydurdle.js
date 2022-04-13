c76f52945bc8131c0c9e622b11c154785ac2bb9e
Tobybsmith <tobybsmith121@gmail.com>
2022-04-13 02:53:05 -0400

    started steely durdle

---
+++ projects/scripts/steelydurdle.js
@@ -0,0 +1,9 @@
+console.log("Hello, World")
+
+function processInput()
+{
+    const text = document.getElementById("entry")
+    var input = text.value
+    //compare to the current steely dan song
+    console.log(input)
+}
